01cf6cd742093f0b89a8e4325df4e51f26f88ba7
Herman Tiutiunyk <herman.tyutyunyk@gmail.com>
2016-10-02 02:57:23 +0300

    description added

--- basic/views/project/index.php
+++ basic/views/project/index.php
@@ -6,3 +6,15 @@ $project = isset($project) ? $project : null;
 <h1>
     <?=$project->title;?>
 </h1>
+
+<h2>
+    <details>
+        <summary>Описание проекта</summary>
+        <?=$project->description;?>
+    </details>
+</h2>
+<hr>
+
+<div class="wrapper row">
+
+</div>
