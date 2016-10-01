ad3fb58bff8549d4b065b81c5c09a7fd63dfdb5c
Maxim Kozachenko <kazak1377@gmail.com>
2016-10-02 01:46:49 +0300

    enabled debug tools

--- basic/config/web.php
+++ basic/config/web.php
@@ -59,6 +59,7 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs' => ['*'],
     ];
 
     $config['bootstrap'][] = 'gii';
