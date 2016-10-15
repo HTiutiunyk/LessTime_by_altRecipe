024da0d6c2f0c69c526db61fcc8317ee018fcb8e
Herman Tiutiunyk <herman.tyutyunyk@gmail.com>
2016-10-16 00:50:19 +0300

    created dashboard with all user's projects

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -1,53 +1,24 @@
 <?php
+/** @var \app\db\Projects[] $projects */
+$projects = isset($projects) ? $projects : null;
 
 /* @var $this yii\web\View */
+$this->title = 'Dashboard';
 
-$this->title = 'My Yii Application';
 ?>
 <div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+    <?php foreach ($projects as $project):?>
+        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+            <a href="<?= \yii\helpers\Url::to(['/project', 'id' => $project->id]) ?>">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <strong><?= $project->title ;?></strong>
+                    </div>
+                    <div class="panel-body">
+                        <p><?= $project->description; ?></p>
+                    </div>
+                </div>
+            </a>
         </div>
-
-    </div>
+    <?php endforeach;?>
 </div>
